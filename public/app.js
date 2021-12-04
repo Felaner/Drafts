4617b3a80a2d7fe356b9232f05ca4d9dc283f189
Arseniy <belovas01@mail.ru>
2021-12-04 16:49:22 +0700

    565454

--- public/app.js
+++ public/app.js
@@ -42,3 +42,5 @@ $(function (){
         }
     })
 })
+
+!function(d,s){var js,fjs=d.getElementsByTagName(s)[0];js=d.createElement(s);js.async=1;js.src="//widget.flamp.ru/loader.js";fjs.parentNode.insertBefore(js,fjs);}(document,"script");
